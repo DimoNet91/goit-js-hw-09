94fab132654ad1876d04cad822050f42aeb1eeca
Dmitriy <malish3250@gmail.com>
2024-05-20 13:10:24 +0300

    upd 1

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+function restoreData() {
+  const data = loadFromLS(STORAGE_KEY) || {};
+
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+}
+
+form.addEventListener('input', () => {
+  const userEmail = form.elements.email.value.trim();
+  const userMessage = form.elements.message.value.trim();
+
+  const data = {
+    email: userEmail,
+    message: userMessage,
+  };
+
+  saveToLS(STORAGE_KEY, data);
+});
+
+form.addEventListener('submit', e => {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  if (email === '' || message === '') {
+    alert('Please fill in all the fields');
+    e.preventDefault();
+  } else {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.clear();
+    form.reset();
+  }
+});
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key = 'empty') {
+  const data = localStorage.getItem(key);
+  try {
+    const result = JSON.parse(data);
+    return result;
+  } catch {
+    return data;
+  }
+}
+
+restoreData();
